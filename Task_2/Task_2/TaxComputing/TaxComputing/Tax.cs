3eedfffe1b3ff3403a17a2dd06d34229ef7610c5
valerabad <valerabaditsa@gmail.com>
2017-01-17 18:34:13 +0200

    Add files via upload

---
+++ Task_2/Task_2/TaxComputing/TaxComputing/Tax.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxComputing
+{
+    public class Tax
+    {
+        public enum Country
+        {
+            USA,
+            Ukraine,
+            Norway
+        }
+        //public short PerсentByCountry(Country country)
+        //{
+        //    short percent = 0;
+        //    switch (country)
+        //    {
+        //        case Country.Norway:
+        //            { percent = 30; }
+        //            break;
+        //        case Country.USA:
+        //            { percent = 10; }
+        //            break;
+        //        case Country.Ukraine:
+        //            { percent = 15; }
+        //            break;
+        //    }
+        //    return percent;
+        //}
+
+        public short PerсentByCountry(Country country, out short additionalPersent)
+        {
+            additionalPersent = 0;
+            short percent = 0;
+            switch (country)
+            {
+                case Country.Norway:
+                    { percent = 30; }
+                    break;
+                case Country.USA:
+                    { percent = 10; }
+                    break;
+                case Country.Ukraine:
+                    {
+                        percent = 15;
+                        additionalPersent = 3;
+                    }
+                    break;
+                default:
+                    additionalPersent = 0;
+                    break;
+            }
+            return percent;
+        }
+    }
+}
